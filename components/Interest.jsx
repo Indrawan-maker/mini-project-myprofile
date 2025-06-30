569b4676e75a18bde53292017df95ea1fa0872c4
Indrawan <indrawanismail0@gmail.com>
2025-07-01 02:41:01 +0800

    update interest

--- components/Interest.jsx
+++ components/Interest.jsx
@@ -4,7 +4,7 @@ export default function Interest() {
         <div className="interest-wrapper">
         <h2>Interest</h2>
         <p>
-            i'm interest in kopi kapal api, nasi pecel in the morning, and lalapan
+            i'm interest in black coffe, Nasi Pecel on the morning weekend, and Lalapan.
         </p>
         </div>
         </>
